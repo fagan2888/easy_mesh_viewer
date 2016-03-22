67ed76f5d3f889ab6b234f323b6e71febd367a2d
fxing79 <xingzhutian@gmail.com>
2016-03-22 21:42:01 +0800

    Initial Commit

---
+++ usemac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  usemac
+//
+//  Created by XingZhutian on 22/03/2016.
+//  Copyright (c) 2016 XingZhutian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
